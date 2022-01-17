0718952a63ef16bca41e76c916e9d1145e3e648c
Roger Peppe <rogpeppe@gmail.com>
2022-01-17 18:05:38 +0000

    better example

--- example_test.go
+++ example_test.go
@@ -2,6 +2,8 @@ package retry_test
 
 import (
 	"fmt"
+	"log"
+	"math/rand"
 	"time"
 
 	"github.com/rogpeppe/retry"
@@ -10,25 +12,40 @@ import (
 type Foo struct{}
 
 func Example() {
-	getFoo()
+	log.SetFlags(log.Lmicroseconds)
+	_, err := getFooWithRetry()
+	if err != nil {
+		log.Printf("getFooWithRetry: %v", err)
+	} else {
+		log.Printf("getFooWithRetry: ok")
+	}
 }
 
 var retryStrategy = retry.Strategy{
-	Delay:       time.Millisecond,
-	MaxDelay:    time.Second,
-	MaxDuration: time.Minute,
+	Delay:       100 * time.Millisecond,
+	MaxDelay:    5 * time.Second,
+	MaxDuration: 10 * time.Second,
+	Factor:      2,
 }
 
 // getFooWithRetry demonstrates a retry loop.
 func getFooWithRetry() (*Foo, error) {
 	for i := retryStrategy.Start(nil); i.Next(); {
-		if foo, err := getFoo(); err == nil {
+		log.Printf("getting foo")
+		foo, err := getFoo()
+		if err == nil {
 			return foo, nil
 		}
+		if !i.HasMore() {
+			return nil, fmt.Errorf("error getting foo after %d tries: %v", i.Count(), err)
+		}
 	}
-	return nil, fmt.Errorf("too many retries")
+	panic("unreachable")
 }
 
 func getFoo() (*Foo, error) {
-	return &Foo{}, nil
+	if rand.Intn(5000) == 0 {
+		return &Foo{}, nil
+	}
+	return nil, fmt.Errorf("some error")
 }
